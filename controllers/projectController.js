8afcbfdbd08f37d7879ce5a1fa0103728931c3a8
song-o <songmi.ohh@gmail.com>
2020-09-06 21:11:04 +0900

    knex로 only project post 성공

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,71 +1,70 @@
-const models = require("../models");
+const knex = require("../db/knex");
 var generateSafeId = require("generate-safe-id");
 
 let projectController = {};
 
 projectController.create = function (req, res) {
-  console.log(req.body);
-  models.project
-    .create({
-      description: req.body.params.description,
-      projectName: req.body.params.projectName,
-      privacy: req.body.params.privacy,
-      api_key: req.body.params.api_key,
+  knex('project')
+    .insert({
+    description: req.body.description,
+    projectName: req.body.projectName,
+    privacy: req.body.privacy,
+    api_key: req.body.api_key,
     })
     .then(() => {
       res.send("A project is created");
     });
 };
 
-projectController.read = function (req, res) {
-  let pageNum = req.body.page;
-  let offset = 0;
-  if (pageNum > 1) {
-    offset = 6 * (pageNum - 1);
-  }
+// projectController.read = function (req, res) {
+//   let pageNum = req.body.page;
+//   let offset = 0;
+//   if (pageNum > 1) {
+//     offset = 6 * (pageNum - 1);
+//   }
 
-  models.project
-    .findAll({
-      offset: offset,
-      limit: 6,
-    })
-    .then((projectList) => {
-      res.json(projectList);
-    });
-};
+//   knex('project').select
+//     .findAll({
+//       offset: offset,
+//       limit: 6,
+//     })
+//     .then((projectList) => {
+//       res.json(projectList);
+//     });
+// };
 
-projectController.getkey = function (req, res) {
-  res.send(generateSafeId());
-};
+// projectController.getkey = function (req, res) {
+//   res.send(generateSafeId());
+// };
 
-projectController.delete = function (req, res) {
-  models.project
-    .destroy({
-      where: {
-        project_id: req.params.id,
-      },
-    })
-    .then(() => {
-      res.end("delete success");
-    });
-};
+// projectController.delete = function (req, res) {
+//   models.project
+//     .destroy({
+//       where: {
+//         project_id: req.params.id,
+//       },
+//     })
+//     .then(() => {
+//       res.end("delete success");
+//     });
+// };
 
-projectController.update = function (req, res) {
-  models.project
-    .update(
-      {
-        description: req.body.description,
-        project_name: req.body.project_name,
-      },
-      {
-        where: {
-          project_id: req.params.id,
-        },
-      }
-    )
-    .then(() => {
-      res.end("update success");
-    });
-};
+// projectController.update = function (req, res) {
+//   models.project
+//     .update(
+//       {
+//         description: req.body.description,
+//         project_name: req.body.project_name,
+//       },
+//       {
+//         where: {
+//           project_id: req.params.id,
+//         },
+//       }
+//     )
+//     .then(() => {
+//       res.end("update success");
+//     });
+// };
 
 module.exports = projectController;
--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -8,14 +8,14 @@ const eachproject = require("../controllers/eachpjController.js");
 
 router.post("/project", project.create);
 
-router.get("/project/key", project.getkey);
+// router.get("/project/key", project.getkey);
 
-router.get("/project", project.read);
+// router.get("/project", project.read);
 
-router.get("/project/:id", eachproject.read);
+// router.get("/project/:id", eachproject.read);
 
-router.delete("/project/:id", project.delete);
+// router.delete("/project/:id", project.delete);
 
-router.put("/project/:id", project.update);
+// router.put("/project/:id", project.update);
 
 module.exports = router;
