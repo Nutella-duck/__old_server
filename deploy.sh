9e7f288fcd3782981b5e35bc8ecb1c603190b111
kitoha <kitoha@daum.net>
2020-09-10 02:00:02 +0900

    test setting

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+echo "nvm 설정"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 echo "해당 디렉토리 이동"
 
 cd /home/ec2-user/delpoy/WebApiServer
