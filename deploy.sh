58ed037a6f2134c7bd497d4c9760cbda4ebeaca3
kitoha <kitoha@daum.net>
2020-09-10 01:47:39 +0900

    fix delpoy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+echo "nvm 설정"
+
+export NVM_DIR="/home/ec2-user/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
 echo "해당 디렉토리 이동"
 
-cd /home/ec2-user/delpoy
+cd /home/ec2-user/delpoy/WebApiServer
+
+echo "package 설치"
+
+npm install
+
+
+npm start
