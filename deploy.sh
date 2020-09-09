db61c7abdcd01a7a3ca2d00626b213082a4e7ca7
kitoha <kitoha@daum.net>
2020-09-10 01:32:24 +0900

    deploy test

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+echo "nvm 설정"
+
+export NVM_DIR="/home/ec2-user/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+echo "해당 디렉토리 이동"
 
 cd /home/ec2-user/delpoy
+
+echo "package 설치"
+
 npm install
+
+
 npm start
