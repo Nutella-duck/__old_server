9d887f1863d20200f4be3f625a3de88f115fe82f
song-o <songmi.ohh@gmail.com>
2020-09-12 23:51:45 +0900

    modify seed_dafault.js

--- seeds/seed_default.js
+++ seeds/seed_default.js
@@ -24,10 +24,10 @@ exports.seed = function(knex) {
               {stepNumber: 1, runId: 2, accuracy: 0.70, loss: 2.8}, 
               {stepNumber: 2, runId: 2, accuracy: 0.75, loss: 2.5}, 
               {stepNumber: 3, runId: 2, accuracy: 0.78, loss: 2.1}, 
-              {stepNumber: 1, runId: 2, accuracy: 0.90, loss: 1.8}, 
-              {stepNumber: 2, runId: 3, accuracy: 0.95, loss: 1.5}, 
-              {stepNumber: 3, runId: 3, accuracy: 0.98, loss:1.1}, 
               {stepNumber: 1, runId: 3, accuracy: 0.90, loss: 1.8}, 
+              {stepNumber: 2, runId: 3, accuracy: 0.95, loss: 1.5}, 
+              {stepNumber: 3, runId: 3, accuracy: 0.98, loss: 1.1}, 
+              {stepNumber: 1, runId: 4, accuracy: 0.90, loss: 1.8}, 
               {stepNumber: 2, runId: 4, accuracy: 0.95, loss: 1.5},  
           ])
       })
