5afa5aa608c5ecb8d1d3ba715e40bd3ed5851c77
song-o <songmi.ohh@gmail.com>
2020-09-16 23:06:53 +0900

    env = development or production

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,9 @@
 const dotenv = require("dotenv");
 const path = require("path");
-const env = process.env.NODE_ENV || 'local';
+const env = process.env.NODE_ENV || 'development';
 
 let configData =
-  env == "local"
+  env == "development"
     ? require("./properties/LocalConfig.json")
     : require("./properties/ServerConfig.json");
 
@@ -11,6 +11,15 @@ dotenv.config({ path: path.join(__dirname, configData.dir) });
 
 module.exports = {
   development: {    
+    client: 'mysql2',
+    connection: {
+      host : process.env.DB_HOST,
+      user : process.env.DB_USER,
+      password : process.env.DB_PASSWORD,
+      database : process.env.DB_NAME,
+    }
+  },
+  production: {    
     client: 'mysql2',
     connection: {
       host : process.env.DB_HOST,
