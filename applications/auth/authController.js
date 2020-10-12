3b6fd342ac70db7bb1ce93159557620c1ef5f64d
kitoha <kitoha@daum.net>
2020-10-12 18:09:32 +0900

    로그인 개발

--- applications/auth/authController.js
+++ applications/auth/authController.js
@@ -25,6 +25,16 @@ const userExistsChecker = async (userName) => {
     });
 };
 
+const getUserPassword = async (userName) => {
+  return knex
+    .select("password")
+    .from("user")
+    .where({ userName: userName })
+    .then((result) => {
+      return result;
+    });
+};
+
 const userSaveDatabase = (username, password) => {
   return knex
     .insert({ userName: username, password: password })
@@ -43,7 +53,7 @@ authController.register = async (req, res) => {
   const result = schema.validate(req.body);
 
   if (result.error) {
-    res.status(400).send("이메일 규정이 올바르지 않습니다.");
+    res.status(400).end("이메일 규정이 올바르지 않습니다.");
   }
 
   const { username, password } = req.body;
@@ -52,7 +62,7 @@ authController.register = async (req, res) => {
     const exists = await userExistsChecker(username);
 
     if (exists.length) {
-      return res.status(400).send("유저가 존재합니다.");
+      return res.status(400).end("유저가 존재합니다.");
     }
 
     const hashPassword = await setPassword(password);
@@ -61,10 +71,39 @@ authController.register = async (req, res) => {
 
     return res.send("success");
   } catch (e) {
-    res.status(400).send(e);
+    res.status(400).end(e);
   }
 
   res.end("success");
+};
+
+authController.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.status(400).end("잘못된 아이디 및 비밀번호입니다.");
+  }
+
+  try {
+    const user = await userExistsChecker(username);
+
+    if (!user.length) {
+      res.status(400).end("없는 유저입니다.");
+    }
+    const dbPassword = await getUserPassword(username);
+
+    console.log(dbPassword[0].password);
+
+    const valid = await checkpassword(password, dbPassword[0].password);
+
+    if (!valid) {
+      return res.status(400).end("비밀번호가 올바르지 않습니다.");
+    }
+
+    res.end("로그인 완료");
+  } catch (e) {
+    res.status(400).end(e);
+  }
 };
 
 module.exports = authController;
