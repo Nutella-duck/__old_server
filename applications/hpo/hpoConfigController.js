32089fca77305627eb3c94bc92d03569ca78c786
오송미 <songmi.ohh@gmail.com>
2020-11-19 14:17:10 +0900

    오타 수정

--- applications/hpo/hpoConfigController.js
+++ applications/hpo/hpoConfigController.js
@@ -15,7 +15,7 @@ const gethpoProjcet = async (projectName) => {
 hpoConfigController.bestResult = async (req, res) => {
   const projectId = req.params.id;
   knex
-    .select("bestParmeter")
+    .select("bestParameter")
     .from("hpoConfig")
     .where({ hpoProjectId: projectId })
     .then((result) => {
